f031cae42a7e528e53379b8be3d6654468e9fa83
amazingli <547452904@qq.com>
2021-05-26 14:06:39 +0800

    5.26 14:00

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world 为什么一下一下的 ");
 		System.out.println("再试一次");
+		System.out.println("再再试一次");
 	}
 
 }
