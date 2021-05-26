0d3dfba9acfe5579a9125ff80dc2a564bef614c9
amazingli <547452904@qq.com>
2021-05-26 13:58:50 +0800

    test2

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world 为什么一下一下的");
 	}
 
 }
